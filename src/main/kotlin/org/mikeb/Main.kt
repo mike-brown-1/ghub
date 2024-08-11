d2f6f17d3f2c4aba3377ab5dc3908c81ec24ee4f
Mike Brown <mike.brown.gm@gmail.com>
2024-08-11 08:41:16 -0500

    Add Repo class, return list of Repo, add initial logging support

--- src/main/kotlin/org/mikeb/Main.kt
+++ src/main/kotlin/org/mikeb/Main.kt
@@ -1,8 +1,17 @@
 package org.mikeb.org.mikeb
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.mikeb.api.searchPublicRepos
 
+private val logger = KotlinLogging.logger {}
 fun main() {
-    println("ghub starting")
-    searchPublicRepos("moshi")
+    logger.info {"ghub starting"}
+    val results = searchPublicRepos("configuration language:java language:kotlin stars:>=500 sort:stars")
+    logger.info { "successful: ${results.successful}, total count: ${results.totalCount}, " +
+            "list size: ${results.items.size}"}
+
+    results.items.forEach { repo ->
+        println("name: ${repo.fullName}, language: ${repo.language}, stars: ${repo.stars}, updated: ${repo.updatedAt}")
+        println("    ${repo.description}, ")
+    }
 }
